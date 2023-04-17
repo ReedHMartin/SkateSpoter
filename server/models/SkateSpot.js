8871876cf6a567c24fab307552914488ed3adbdd
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-04-17 19:22:37 -0400

    fix skatespot

--- server/models/SkateSpot.js
+++ server/models/SkateSpot.js
@@ -1,34 +1,33 @@
-const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+const { Schema, model } = mongoose;
 
 const spotSchema = new Schema({
-    location: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    lighting: {
-        type: Number,
-        maxlength: 10,
-    },
-    police_presence: {
-        type: Array,
-        anyOf: [
-            ["Red"],
-            ["Yellow"],
-            ["Green"]
-        ]
-    },
-    pedestrians: {
-        type: Number,
-        maxlength: 10,
-    },
-    typeOf: {
-        type: String,
-        maxlength: 50,
-    }
-})
+  location: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  lighting: {
+    type: Number,
+    maxlength: 10,
+  },
+  police_presence: {
+    type: Array,
+    anyOf: [["Red"], ["Yellow"], ["Green"]],
+  },
+  pedestrians: {
+    type: Number,
+    maxlength: 10,
+  },
+  typeOf: {
+    type: String,
+    maxlength: 50,
+  },
+});
+
+const skateSpot = model("skateSpot", spotSchema);
+
+module.exports = skateSpot;
