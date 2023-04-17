c7ae63634ad0e6a598f6107d5d3347040a88e276
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-04-17 19:44:02 -0400

    add queries

--- client/src/Utils/queries.js
+++ client/src/Utils/queries.js
@@ -1,13 +1,33 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_PROFILE = gql`
-query profile {
+  query profile {
     profile {
-        _id
-        username
-        email
-        skateSpot {
-            
-        }
+      _id
+      username
+      email
+      skateSpot {
+        location
+        name
+        lighting
+        police_presence
+        pedestrians
+        typeof
+      }
     }
-}`;
+  }
+`;
+
+export const QUERY_SKATESPOT = gql`
+  query skateSpot {
+    skateSpot {
+      _id
+      location
+      name
+      lighting
+      police_presence
+      pedestrians
+      typeof
+    }
+  }
+`;
