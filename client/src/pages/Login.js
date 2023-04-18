59d6b0e29d4862ae3025157581fd9ab31c2a32c6
Reed Martin <reedhmartin@gmail.com>
2023-04-18 18:59:28 -0400

    feat: Add basic Login component with form and input handlers

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function () {
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic, e.g., call API to authenticate the user
+    console.log("Username:", username, "Password:", password);
+  };
+
   return (
     <div>
-      <h2>login</h2>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
